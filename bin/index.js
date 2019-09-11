1a8c46658f12af69bbcc1aec0fb8fa9a682f21b4
Leandro Vilas Boas <leandro.vboas@lambda3.com.br>
2019-09-11 13:15:44 -0300

    Alterado estrutura do projeto

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,8 @@
 const program = require('commander')
 
 const config = require('../config')
-const user = require('./cmds/users')
-const repos = require('./cmds/repos')
+const user = require('./cmds/usersCmd')
+const repos = require('./cmds/reposCmd')
 
 program.version(config.version)
 
@@ -12,9 +12,9 @@ program
     .command('user <userName>')
     .description('Busca informações do usuario na api do github')
     .option('-r, --repos [repos]', 'Retorna uma lista com os repositórios do usuário')
-    .option('-t, --table [table]', 'Combinado com o -r retorna uma tabela com os repositórios do usuário')
+    //.option('-t, --table [table]', 'Combinado com o -r retorna uma tabela com os repositórios do usuário')
     .action((userName, options) => {
-        user(userName, options.repos, options.table)  
+        user(userName, options.repos)  
     });
 
 program
