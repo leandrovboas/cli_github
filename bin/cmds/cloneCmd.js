30b815bb8b9ad0956696aaa97a931f291f38827b
Leandro Vilas Boas <leandro.vboas@lambda3.com.br>
2019-09-16 15:21:36 -0300

    Incluindo clone para caminho especifico

--- bin/cmds/cloneCmd.js
+++ bin/cmds/cloneCmd.js
@@ -2,18 +2,22 @@ const shell = require('shelljs');
 
 const reposService = require('../../src/services/reposService')
 
-module.exports = async (userName, repoName) => {
+module.exports = async (userName, repoName, optionPath) => {
     if (!shell.which('git')) {
-        shell.echo('Sorry, this script requires git');
+        shell.echo('Desculpe, mas esse script requer o git instalado');
         shell.exit(1);
     }
 
     let repo = await reposService.GetRepo(userName, repoName)
 
+    if(optionPath !== null){
+        shell.cd(optionPath)
+    }
+
     if (shell.exec(`git clone ${repo.clone_url}`).code === 0) {
         shell.echo(`Repositório ${repo.name} Clonado com sucesso`)
     }else{
-        shell.echo('Error: Git clone failed');
+        shell.echo('Error: Git clone falhou');
         shell.exit(1);
     }
 
--- bin/index.js
+++ bin/index.js
@@ -10,38 +10,48 @@ const clone = require('./cmds/cloneCmd')
 program.version(config.version)
 
 program
-    .command('user <userName>')
-    .description('Busca informações do usuario na api do github')
-    .option('-r, --repos [repos]', 'Retorna uma lista com os repositórios do usuário')
-    .option('-j, --json [json]', 'Retorna as informações solicitadas no formato jsom')
-    //.option('-t, --table [table]', 'Combinado com o -r retorna uma tabela com os repositórios do usuário')
-    .action((userName, options) => {
-        user(userName, options.repos, options.json)  
-    });
-
-program
-    .command('repos <userName>')
-    .description('Busca os repopsitórios do usuario na api do github')
-    .option('-j, --json [json]', 'Retorna as informações solicitadas no formato jsom')
-    .action((userName, option) => {
-        repos(userName, option.json)  
-    });
-
-program
-    .command('clone <userName> <RepoName>')
-    .description('Realiza um git clone do repositório informado')
-    .option('-p, --path <path>', 'Realiza um git clone para o caminha informado','')
-    .action((userName, nameRepo) => {
-        //program.parse(process.argv);
-        //console.log(`path: ${program.path}`);
-        clone(userName, nameRepo)
-    });
-
-program.on('--help', function(){
-  console.log('')
-  console.log('Examples:');
-  console.log('  $ custom-help --help');
-  console.log('  $ custom-help -h');
+.command('user <userName>')
+.description('Busca informações do usuario na api do github')
+.option('-r, --repos [repos]', 'Retorna uma lista com os repositórios do usuário')
+.option('-j, --json [json]', 'Retorna as informações solicitadas no formato jsom')
+//.option('-t, --table [table]', 'Combinado com o -r retorna uma tabela com os repositórios do usuário')
+.action((userName, options) => {
+    user(userName, options.repos, options.json)  
 });
 
-program.parse(process.argv);
+program
+.command('repos <userName>')
+.description('Busca os repopsitórios do usuario na api do github')
+.option('-j, --json [json]', 'Retorna as informações solicitadas no formato jsom')
+.action((userName, option) => {
+    repos(userName, option.json)  
+});
+
+program
+.command('clone <userName> <RepoName>')
+.description('Realiza um git clone do repositório informado')
+.option('-p, --path <type>', 'Realiza um git clone para o caminha informado')
+.action((userName, nameRepo, option) => {
+    let p = option.rawArgs.indexOf('-p')
+    let path = option.rawArgs.indexOf('--path')
+    if((p > 0 || path > 0) && option.path === undefined){
+        console.error('Para a opção informada deve ser passado um argumento')
+        return
+    }
+  clone(userName, nameRepo, option.path)
+});
+
+program.on('--help', function(){
+    console.log('')
+    console.log('Examples:');
+    console.log('  $ github user ----------------------------------->')
+    console.log('  $ github user leandrovboas');
+    console.log('  $ github repos ----------------------------------->')
+    console.log('  $ github repos leandrovboas');
+    console.log('  $ github clone ----------------------------------->')
+    console.log('  $ github clone leandrovboas cli_github --path=CAMINHO_PARA_CLONAR');
+    console.log('  $ github clone leandrovboas cli_github --path CAMINHO_PARA_CLONAR');
+    console.log('  $ github clone leandrovboas cli_github -p CAMINHO_PARA_CLONAR');
+});
+
+program.parse(process.argv)
