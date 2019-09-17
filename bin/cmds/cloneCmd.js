c9a1cb483e14106d3c30a534adc0312e6c2ef64b
Leandro Vilas Boas <leandro.vboas@lambda3.com.br>
2019-09-17 16:13:46 -0300

    retirando metodo nao usado

--- bin/cmds/cloneCmd.js
+++ bin/cmds/cloneCmd.js
@@ -11,10 +11,13 @@ module.exports = async (userName, repoName, optionPath) => {
     let repo = await reposService.GetRepo(userName, repoName)
 
     if(optionPath !== undefined){
+        if(!shell.test('-e', optionPath)) {
+            console.error(`Caminho informado não pode ser encontrado ===> ${optionPath}`)
+            shell.exit(1)
+        }
         shell.cd(optionPath)
     }
 
-    shell.ls()
     if (shell.exec(`git clone ${repo.clone_url}`).code === 0) {
         shell.echo(`Repositório ${repo.name} Clonado com sucesso`)
     }else{
